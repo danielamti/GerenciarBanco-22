1deb5a2f504d1d76c0440ffb27b492491409e266
danielamti <130715149+danielamti@users.noreply.github.com>
2024-04-02 00:37:51 +0000

    Initial commit

---
+++ src/main/java/Banco.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+class Banco {
+  private String nome;
+  private String sobrenome;
+  private String cpf;
+  private float saldo;
+
+  Scanner ler = new Scanner(System.in);
+
+
+
+
+public String getNome() {
+  return nome;
+}
+public void setNome(String nome) {
+  this.nome = nome;
+}
+public String getSobrenome() {
+  return sobrenome;
+}
+public void setSobrenome(String sobrenome) {
+  this.sobrenome = sobrenome;
+}
+public String getCpf() {
+  return cpf;
+}
+public void setCpf(String cpf) {
+  this.cpf = cpf;
+}
+
+  public float getSaldo() {
+    return saldo;
+  }
+  public void setSaldo(float saldo) {
+    this.saldo = saldo;
+  }
+
+
+
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {      
+
+    menu();
+  }
+
+
+  public static void menu(){    
+    Banco bank = new Banco();
+    Scanner ler = new Scanner(System.in);
+    // cadastro
+    System.out.println("Olá bem vindo ao  MarxBank\n\n");
+    System.out.println("Insira seu nome: ");
+    bank.setNome(ler.nextLine());
+    System.out.println("Insira o sombrenome: ");
+    bank.setSobrenome(ler.nextLine());
+    System.out.println("Insira o CPF: ");
+    bank.setCpf(ler.nextLine());
+    System.out.println("Obrigado pelo cadastro "+ bank.getNome());
+
+
+    //cadastro fim
+
+
+
+
+    int indicador;
+    indicador = 0;  
+
+    System.out.println("Ola "+ bank.getNome());
+    System.out.println("Escolha uma das opções:\n1 - Saldo\n2 - Deposito\n3 - Saque\n4 - Sair");
+    while (indicador != 4){
+    indicador = ler.nextInt();
+
+    switch (indicador){
+      case 1:
+        System.out.println("Seu saldo é de R$ "+ bank.getSaldo());
+        break;
+      case 2:
+        System.out.println("Insira um valor de depósito: R$ ");
+        bank.setSaldo(ler.nextFloat() + bank.getSaldo());
+        System.out.println("Seu saldo agora é R$ "+ bank.getSaldo());
+        break;
+      case 3:
+        float saque;
+        System.out.println("Insira o valor do Saque: ");
+        saque = ler.nextFloat();
+        if (bank.getSaldo() >= saque){
+          bank.setSaldo(bank.getSaldo() - saque);
+          System.out.println("Seu saldo atual é R$ "+ bank.getSaldo());
+        }else{
+          System.out.println("Saldo Insuficiente");
+
+        }
+          break;
+       case 4:
+        break;
+
+    }
+      }
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
